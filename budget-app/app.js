1b8a922c8f6fc9facfa936d16e540c9553ce6b84
knubbe <nikola@42coders.com>
2018-08-10 21:50:28 +0200

    clear the fields

--- budget-app/app.js
+++ budget-app/app.js
@@ -99,6 +99,18 @@ var UIController = (function() {
 
 		},
 
+		clearFields: function() {
+			var fields, fieldsArr;
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			fieldsArr.forEach(function(current, index, array) {
+				current.value = "";
+			});
+			fieldsArr[0].focus();
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -130,9 +142,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 		// 3. Add the item to the UI
 		UICtrl.addListItem(newItem, input.type);
-		// 4. Calculate the budget
+		// 4. Clear the fields
+		UICtrl.clearFields();
+		// 5. Calculate the budget
 
-		// 5. Display the budget on the UI
+		// 6. Display the budget on the UI
 		
 	};
 
