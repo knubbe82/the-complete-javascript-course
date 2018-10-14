91f65362d4c71f5f5c6c69d7af3be12d104dcf6b
knubbe82 <knubbe1982@gmail.com>
2018-10-14 20:17:01 +0200

    delete item finish

--- budget-app/app.js
+++ budget-app/app.js
@@ -154,6 +154,11 @@ var UIController = (function() {
 
 		},
 
+		deleteListItem: function(selectorID) {
+			var el = document.getElementById(selectorID);
+			el.parentNode.removeChild(el);
+		},
+
 		clearFields: function() {
 			var fields, fieldsArr;
 			fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
@@ -246,8 +251,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 			// 1. delete the item from the data structure
 			budgetCtrl.deleteItem(type, ID);
 			// 2. Delete the item from the UI
-
+			UICtrl.deleteListItem(itemID);	
 			// 3. Update and show the new budget
+			updateBudget();
 		}
 	}
 
